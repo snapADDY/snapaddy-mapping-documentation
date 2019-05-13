9631f9731058cd37d0c7f6b5794a12f9cea71fa7
Maximilian Stock <me@maximumstock.net>
2019-05-13 12:00:53 +0200

    chore(): Add index.js to export JSON files

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  duplicateCheck: {
+    dynamics: require('./Duplicate Check/Dynamics Duplicate Check v4.json'),
+    sap_c4c: require('./Duplicate Check/SAP Duplicate Check v4.json'),
+    salesforce: require('./Duplicate Check/Salesforce Duplicate Check v4.json'),
+  }
+}
